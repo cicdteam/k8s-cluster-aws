5966fb306789fc23375c463a759220f3542c2b3b
Andrew Taranik <me@pureclouds.net>
2017-06-19 05:22:09 +0000

    AMI creating moved to separate github repo

--- k8s-worker/ami.tf
+++ k8s-worker/ami.tf
@@ -1,10 +1,11 @@
 # AMI for Kubernetes 1.6 node with GPU support
+# https://github.com/pureclouds/k8s-ami
 #
 data "aws_ami" "image" {
   most_recent = true
   filter {
     name = "name"
-    values = ["k8s-1.6-ubuntu-xenial-amd64-hvm-ebs-*"]
+    values = ["k8s-1.6-gpu-ubuntu-xenial-amd64-hvm-ebs"]
   }
   filter {
     name = "virtualization-type"
